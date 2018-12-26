3832e21d270c5c5f349038df2d458df68058ab15
hardianrachadi123 <rachmadihardian@gmail.com>
2018-12-26 22:01:17 +0700

    projekk

--- application/config/config.php
+++ application/config/config.php
@@ -23,10 +23,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'http://localhost/projek';
 
 /*
 |--------------------------------------------------------------------------
+| Index File
 | Index File
 |--------------------------------------------------------------------------
 |
